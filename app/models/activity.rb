963e3e4aef0e937260996d6910dac3d6eb612615
Yusuke Nakamura <nakamura@kogakusha.co.jp>
2018-06-12 11:44:36 +0900

    includes.orderの記述を修正

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
   belongs_to :event
   belongs_to :customer
-  default_scope { includes(:customer).order('customer.kana COLLATE "ja_JP.utf8" ASC') }
+  default_scope { includes(:customer).order('customers.kana COLLATE "ja_JP.utf8" ASC') }
 end
--- app/models/event_detail.rb
+++ app/models/event_detail.rb
@@ -1,5 +1,5 @@
 class EventDetail < ApplicationRecord
   belongs_to :event
   belongs_to :advisor
-  default_scope { includes(:advisor).order('advisor.kana COLLATE "ja_JP.utf8" ASC') }
+  default_scope { includes(:advisor).order('advisors.kana COLLATE "ja_JP.utf8" ASC') }
 end
